6e49a7fc1e84d3afb4c377e024493710549e04d2
YasamanFarhangi <yf7901@gmail.com>
2025-01-07 14:35:26 +0330

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7e808797-4050-4557-9049-509994ff649b';
+export const MODAL_CLOSE_SEC = 2.5;
