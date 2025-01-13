086ee3d72ffde509f798a4fef9fec955bab15a2d
YasamanFarhangi <yf7901@gmail.com>
2025-01-13 13:57:10 +0330

    fixing the icons error

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SECONDS = 10;
+export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '7e808797-4050-4557-9049-509994ff649b';
+export const KEY = '<YOUR_KEY>';
 export const MODAL_CLOSE_SEC = 2.5;
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,5 +1,5 @@
 import { async } from 'regenerator-runtime';
-import { TIMEOUT_SECONDS } from './config.js';
+import { TIMEOUT_SEC } from './config.js';
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -21,7 +21,7 @@ export const AJAX = async function (url, uploadData = undefined) {
         })
       : fetch(url);
 
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SECONDS)]);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -34,7 +34,8 @@ export const AJAX = async function (url, uploadData = undefined) {
 /*
 export const getJSON = async function (url) {
   try {
-    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SECONDS)]);
+    const fetchPro = fetch(url);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -44,7 +45,7 @@ export const getJSON = async function (url) {
   }
 };
 
-export const sentJSON = async function (url, uploadData) {
+export const sendJSON = async function (url, uploadData) {
   try {
     const fetchPro = fetch(url, {
       method: 'POST',
@@ -53,7 +54,8 @@ export const sentJSON = async function (url, uploadData) {
       },
       body: JSON.stringify(uploadData),
     });
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SECONDS)]);
+
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
     const data = await res.json();
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
